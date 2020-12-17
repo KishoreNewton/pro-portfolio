171c5734e152c5584745ffdf2ac6382e7de013cd
Kishore Newton <kishorenke@gmail.com>
2020-12-17 06:48:40 +0530

    feat: choose the color

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
 import '../css/main.css';
 
-document.getElementById('search').addEventListener('change', event => {
-  console.log('working');
+function changeStyle() {
+  document.body.style.background = `linear-gradient(
+    90deg,
+    #ddd6f3,
+    #fdf5f5,
+    #fff3f3,
+    #ffefef,
+    #ffe7e7,
+    #ffe7e7,
+    #fcd4d4,
+    #eeb3b0,
+    #faaca8
+  )`;
+  document.body.style.animation = `rainbow 8s ease infinite`;
+  document.body.style.backgroundSize = `180% 180%`;
+}
+
+document.addEventListener('click', event => {
+  if (event.target.id === 'search') {
+    document.body.style.background = `linear-gradient(
+      90deg,
+      #fdbebe,
+      #e7caa3,
+      #e7d8a5,
+      #bde2a4,
+      #aef0e1,
+      #a1bae0,
+      #c1a9ee,
+      #eeb1e5,
+      #ff6a83
+    )`;
+    document.body.style.animation = `rainbow 8s ease infinite`;
+    document.body.style.backgroundSize = `180% 180%`;
+  } else {
+    changeStyle();
+  }
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    document.getElementById('search').blur();
+    changeStyle();
+  }
 });
