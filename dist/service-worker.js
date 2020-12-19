2d03ad898c356f28ea56c5c3d9f0a60e798779fe
Kishore Newton <kishorenke@gmail.com>
2020-12-19 12:37:15 +0530

    feat: updated

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -2,14 +2,14 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox
 workbox.routing.registerRoute(({ request }) => request.destination === 'image', new workbox.strategies.NetworkFirst());
 
 var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = ['/', '/main.bundle.js', '/icons', '/avatar.jpeg', '/favicon.png', '/index.html', '/favicon.ico'];
+var urlsToCache = ['/', '/main.bundle.js', '/icons/*', '/avatar.jpeg', '/favicon.png', '/index.html', '/favicon.ico'];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', async function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('Opened cache');
-      return cache.addAll(urlsToCache);
+      return await cache.addAll(urlsToCache);
     })
   );
 });
